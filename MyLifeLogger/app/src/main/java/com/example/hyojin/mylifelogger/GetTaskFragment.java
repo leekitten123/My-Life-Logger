566c9da32fd5860c631c6f9f1b76f09fc3c254da
LeeSangWook <leekitten123@hanmail.net>
2016-11-27 21:39:57 +0900

    add StopWatch

--- MyLifeLogger/app/src/main/java/com/example/hyojin/mylifelogger/GetTaskFragment.java
+++ MyLifeLogger/app/src/main/java/com/example/hyojin/mylifelogger/GetTaskFragment.java
@@ -6,6 +6,7 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.SystemClock;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,10 +26,21 @@ public class GetTaskFragment extends Fragment {
     int iMonth = Calendar.getInstance().get(Calendar.MONTH) + 1;
     int iDate = Calendar.getInstance().get(Calendar.DAY_OF_MONTH) ;
 
-    /** 타이머에 사용할 핸들러 **/
+    /** 스톱워치에 사용할 변수 **/
+    statusStopWatch cur_Status = statusStopWatch.Init ;
+    int myCount = 1 ;
+    long myBaseTime ;
+    long myPauseTime ;
+
+    /** 스톱워치에 사용할 뷰 **/
+    TextView textPrintTime ;
+    Button btnTimeStatus ;
+    Button btnTimeRecord ;
+
+    /** 스톱워치에 사용할 핸들러 **/
     Handler myTimer = new Handler(){
         public void handleMessage(Message msg){
-            //myOutput.setText(getTimeOut());
+            textPrintTime.setText(getTimeOut());
             myTimer.sendEmptyMessage(0);
         }
     };
@@ -65,6 +77,66 @@ public class GetTaskFragment extends Fragment {
             }
         });
 
+        /** 스톱워치를 작동시키는 리스너 **/
+        textPrintTime = (TextView) view.findViewById(R.id.text_PrintTime);
+        btnTimeStatus = (Button) view.findViewById(R.id.btn_TimeStatus);
+        btnTimeStatus.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                switch (cur_Status) {
+                    case Init:
+                        myBaseTime = SystemClock.elapsedRealtime();
+                        System.out.println(myBaseTime);
+                        myTimer.sendEmptyMessage(0);
+                        btnTimeStatus.setText("멈춤");
+                        //myBtnRec.setEnabled(true); //기록버튼 활성
+                        cur_Status = statusStopWatch.Run;
+                        break;
+
+                    case Run:
+                        myTimer.removeMessages(0);
+                        myPauseTime = SystemClock.elapsedRealtime();
+                        btnTimeStatus.setText("시작");
+                        //myBtnRec.setText("리셋");
+                        cur_Status = statusStopWatch.Pause;
+                        break;
+
+                    case Pause:
+                        long now = SystemClock.elapsedRealtime();
+                        myTimer.sendEmptyMessage(0);
+                        myBaseTime += (now - myPauseTime);
+                        btnTimeStatus.setText("멈춤");
+                        //myBtnRec.setText("기록");
+                        cur_Status = statusStopWatch.Run;
+                        break;
+                }
+            }
+        });
+
+        btnTimeRecord = (Button) view.findViewById(R.id.btn_TimeRecord) ;
+        btnTimeRecord.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                switch(cur_Status){
+                    case Run:
+                        String str = String.format("%d. %s\n",myCount,getTimeOut());
+                        Toast.makeText(getActivity().getApplicationContext(), str, Toast.LENGTH_SHORT).show() ;
+                        //myRec.setText(str);
+                        myCount++ ;
+                        break;
+
+                    case Pause:
+                        myTimer.removeMessages(0) ;
+                        btnTimeStatus.setText("시작") ;
+                        btnTimeRecord.setText("초기화") ;
+                        textPrintTime.setText("00:00:00") ;
+                        cur_Status = statusStopWatch.Init ;
+                        myCount = 1 ;
+                        //myRec.setText("");
+                        btnTimeRecord.setEnabled(false);
+                        break;
+                }
+            }
+        });
+
         /** 날짜 정보를 받는 이벤트 리스너 **/
         textViewCalenderTask.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -85,7 +157,7 @@ public class GetTaskFragment extends Fragment {
             }
         });
 
-        return view ;
+        return view;
     }
 
     /** 위치 정보를 가져오는 메서드 **/
@@ -101,5 +173,17 @@ public class GetTaskFragment extends Fragment {
         }
 
         Toast.makeText(getActivity().getApplicationContext(), "위치 확인", Toast.LENGTH_SHORT).show();
+    }
+
+    /** 스톱워치의 현재 정보 **/
+    public enum statusStopWatch {Init, Run, Pause}
+
+    /** 현재시간을 계속 구해서 출력하는 메소드 **/
+    public String getTimeOut(){
+        long now = SystemClock.elapsedRealtime() ;
+        long outTime = now - myBaseTime ;
+        String easy_outTime = String.format("%02d:%02d:%02d", (outTime/1000)/60, (outTime/1000)%60, (outTime%1000)/10) ;
+
+        return easy_outTime ;
     }
 }
